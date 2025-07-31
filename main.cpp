2f36da74fbd089a325705445f0ea2a25c28d9057
shravyanshetty <shravyas295@gmail.com>
2025-07-31 19:24:52 +0530

    Implemented event menu

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std();
+int main(){
+    int choice;
+
+
+    cout<<"----MENU----\n";
+    cout<<"1.VIEW EVENTS\n";
+    cout<<"2.FILTER EVENTS BY TYPE\n";
+    cout<<"3.ADD NEW EVENT\n";
+    cout<<"4.EXIT\n";
+
+    cout<<"Enter your choice: ";
+    cin>>choice;
+
+}
